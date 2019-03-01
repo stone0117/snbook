8667deeea33d8c1734cf7ac5b1dbcb1ec53cb2f8
StonePark <blueone009@163.com>
2019-03-01 09:16:57 +0800

    2019-03-01 09:16:57 default commit message

--- cp.sh
+++ cp.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
+gitbook build
+
 cp -R _book/ $HOME/git_repository/snbook/
 
+
+cd /Users/stone/git_repository/snbook
+
+git add --all
+
+git commit -am "`date '+%Y-%m-%d %H:%M:%S'` default commit message"
+
+git push
