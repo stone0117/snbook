357b086a7e7880252f36d5422a1dc2bd14b0ff91
StonePark <blueone009@163.com>
2020-06-24 11:07:39 +0800

    2020-06-24 11:07:38 default commit message

--- sn_unzip.sh
+++ sn_unzip.sh
@@ -1,6 +1,16 @@
 #!/usr/local/bin/bash
 
-cd ./records/
+#cd ./records/
+#
+#for item in *.zip; do
+#    #    echo "item = ${item}"
+#    tmp=${item##*/}
+#    r=${tmp%.*}
+#    unzip -o "$item" -d "${r}"
+#    rm "$item"
+#done
+
+cd $HOME/git_repository/snbook_edit/records
 
 for item in *.zip; do
     #    echo "item = ${item}"
@@ -9,3 +19,5 @@ for item in *.zip; do
     unzip -o "$item" -d "${r}"
     rm "$item"
 done
+
+echo -e "\033[0;32;1m=============================================\033[0m"
--- unzip_publish.sh
+++ unzip_publish.sh
@@ -1,16 +1,16 @@
 #!/usr/local/bin/bash
 
-cd $HOME/git_repository/snbook_edit/records
-
-for item in *.zip; do
-    #    echo "item = ${item}"
-    tmp=${item##*/}
-    r=${tmp%.*}
-    unzip -o "$item" -d "${r}"
-    rm "$item"
-done
-
-echo -e "\033[0;32;1m=============================================\033[0m"
+#cd $HOME/git_repository/snbook_edit/records
+#
+#for item in *.zip; do
+#    #    echo "item = ${item}"
+#    tmp=${item##*/}
+#    r=${tmp%.*}
+#    unzip -o "$item" -d "${r}"
+#    rm "$item"
+#done
+#
+#echo -e "\033[0;32;1m=============================================\033[0m"
 
 cd $HOME/git_repository/snbook_edit
 
